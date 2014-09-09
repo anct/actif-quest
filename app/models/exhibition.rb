665b1ec9d8e902720bf90a6b72bf24622163dfc3
やましー <3.1415mailadress@gmail.com>
2014-09-09 22:26:25 +0900

    Setting Exhibition validations about name, introduction, and group_id

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -1,4 +1,7 @@
 class Exhibition < ActiveRecord::Base
   belongs_to :Group
   mount_uploader :image_url, ExhibitionUploader
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :group_id, presence: true
 end
