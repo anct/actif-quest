f9d997f4f0971c9208225a3022722b4de7c8b241
やましー <3.1415mailadress@gmail.com>
2014-09-10 19:07:39 +0900

    Create Achievement model

---
+++ spec/factories/achievements.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :achievement do
+    name "MyString"
+    condition "MyString"
+    description "MyString"
+    icon_url "MyString"
+  end
+end
