f545e1f3795640e7419fa517d2d76290d2ff45d4
izumin5210 <masayuki@izumin.info>
2014-09-12 00:53:30 +0900

    Modify locale and TZ

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,12 @@
 
 FactoryGirl.define do
   factory :user do
+    Faker::Config.locale = :en
     name Faker::Internet.user_name(5..16).gsub(/\./, '')
-    screen_name Faker::Name.name
+    screen_name {
+      Faker::Config.locale = :ja
+      Faker::Name.name
+    }
     image_url Faker::Internet.url
   end
 end
