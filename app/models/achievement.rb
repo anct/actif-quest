b2d9a585b87cded5bc8bba27a4df4d30e0984979
やましー <3.1415mailadress@gmail.com>
2014-09-13 22:17:52 +0900

    Add mount_uploader

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,4 +1,5 @@
 class Achievement < ActiveRecord::Base
+	mount_uploader :icon_url, AchievementUploader
   validates :name, presence: true
   validates :condition, presence: true
   validates :description, presence: true
