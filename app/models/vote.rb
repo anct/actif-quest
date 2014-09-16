f2373aea725f1b232de285defe0ac25c4898b9e5
izumin5210 <masayuki@izumin.info>
2014-09-17 01:52:15 +0900

    Add annotations to models and specs

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  votable_id   :integer
+#  votable_type :string(255)
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class Vote < ActiveRecord::Base
 
   belongs_to :user
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: achievements
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  condition   :string(255)
+#  description :string(255)
+#  icon_url    :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe Achievement, :type => :model do
