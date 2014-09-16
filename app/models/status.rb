be66a2da910de08b96e9fd0e7bf11d0fff6aff94
やましー <3.1415mailadress@gmail.com>
2014-09-16 21:59:27 +0900

    Statusを表示できるようにした

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: statuses
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  body       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Status < ActiveRecord::Base
 	belongs_to :user
 	default_scope -> { order('created_at DESC') }
