b800d5e80991a778cbfe490aa1e09f9c51532a9c
やましー <3.1415mailadress@gmail.com>
2014-09-16 12:12:06 +0900

    Relate User model with Status model by has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,11 @@
 #
 
 class User < ActiveRecord::Base
-
   has_many :identities
   has_many :votes
   has_many :voted_exhibitions, class_name: Exhibition.name, through: :votes
-
+  has_many :statuses, dependent: :destroy
+  
   validates_presence_of :name, :screen_name
   validates_uniqueness_of :name
   validates_format_of :name, with: /\A(\w)+\Z/
