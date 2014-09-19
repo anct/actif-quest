16561c99f93f02de9dac7c2d4156f69aa7e948e9
やましー <3.1415mailadress@gmail.com>
2014-09-19 15:35:26 +0900

    admin_action_btnヘルパー作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def admin_menu_item(name, path)
 		link_to(name, path, class: "list-group-item")
 	end
+
+	def admin_action_btn(name, path, option = {})
+		link_to(name, path, option.merge(class: "btn btn-primary"))
+	end
 end
