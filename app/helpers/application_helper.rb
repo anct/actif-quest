d16bb0cbf8e7c5e160af06d83317cc28a00e3d50
やましー <3.1415mailadress@gmail.com>
2014-09-19 16:07:31 +0900

    Replace tab to space

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-	def admin_menu_item(name, path)
-		link_to(name, path, class: "list-group-item")
-	end
+  def admin_menu_item(name, path)
+    link_to(name, path, class: "list-group-item")
+  end
 
-	def admin_action_btn(name, path, option = {})
-		link_to(name, path, option.merge(class: "btn btn-primary"))
-	end
+  def admin_action_btn(name, path, option = {})
+    link_to(name, path, option.merge(class: "btn btn-primary"))
+  end
 end
