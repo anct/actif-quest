aef5a85cae0eac21773167bc8410103e577eaf89
izumin5210 <masayuki@izumin.info>
2014-09-21 09:49:18 +0900

    Refactor groups controller spec

--- spec/controllers/admin/groups_controller_spec.rb
+++ spec/controllers/admin/groups_controller_spec.rb
@@ -3,56 +3,30 @@ require 'rails_helper'
 RSpec.describe Admin::GroupsController, type: :controller do
 
   describe 'GET #index' do
-    subject { get :index }
-
-    it { is_expected.to be_success }
+    it_behaves_like 'check status in #index of management screen'
   end
 
   describe 'GET #show' do
-    let(:group) { FactoryGirl.create(:group) }
-    subject { get :show, id: group }
-
-    it { is_expected.to be_success }
+    it_behaves_like 'check status in #index of management screen', Group
   end
 
   describe 'GET #new' do
-    subject { get :new }
-
-    it { is_expected.to be_success }
+    it_behaves_like 'check status in #new of management screen'
   end
 
   describe 'GET #edit' do
-    let(:group) { FactoryGirl.create(:group) }
-    subject { get :edit, id: group }
-
-    it { is_expected.to be_success }
+    it_behaves_like 'check status in #edit of management screen', Group
   end
 
   describe 'POST #create' do
-    let(:group) { FactoryGirl.create(:group) }
-    it do
-      expect {
-        post :create, group: FactoryGirl.attributes_for(:group)
-      }.to change(Group, :count).by(1)
-    end
+    it_behaves_like 'check status in #create of management screen', Group
   end
 
   describe 'PATCH #update' do
-    let!(:group) { FactoryGirl.create(:group) }
-    let!(:group_params) { FactoryGirl.attributes_for(:group) }
-    before { patch :update, id: group, group: group_params }
-    it do
-      group.reload
-      expect(group.name).to eq group_params[:name]
-    end
+    it_behaves_like 'check status in #create of management screen', Group
   end
 
   describe 'DELETE #destroy' do
-    let!(:group) { FactoryGirl.create(:group) }
-    it do
-      expect {
-        delete :destroy, id:group
-      }.to change(Group, :count).by(-1)
-    end
+    it_behaves_like 'check status in #destroy of management screen', Group
   end
 end
