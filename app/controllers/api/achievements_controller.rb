4d93ea2fa49f4c233f9a58bae2caab7721c3af3c
やましー <3.1415mailadress@gmail.com>
2014-09-21 22:12:49 +0900

    Create 'api/achievements_controller'

---
+++ app/controllers/api/achievements_controller.rb
@@ -0,0 +1,7 @@
+class Api::AchievementsController < Api::BaseController
+
+	def index
+		@achievements = Achievement.all
+		render json: @achievements
+	end
+end
---
+++ spec/controllers/api/achievements_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Api::AchievementsController, :type => :controller do
+
+end
