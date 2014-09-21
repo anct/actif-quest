e7b6895721914fc324e7e7dc54f5d0f2b8a5abc0
izumin5210 <masayuki@izumin.info>
2014-09-21 11:16:36 +0900

    Add users controller spec

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Admin::UsersController, :type => :controller do
 
+  describe 'GET #index' do
+    it_behaves_like 'check status in #index of management screen'
+  end
+
+  describe 'GET #show' do
+    it_behaves_like 'check status in #index of management screen', User
+  end
+
+  describe 'DELETE #destroy' do
+    it_behaves_like 'check status in #destroy of management screen', User
+  end
 end
