1e4f8ade7a5edd14623834728d35ff0392096442
やましー <3.1415mailadress@gmail.com>
2014-09-23 09:55:46 +0900

    bounds#showにbootstrap適用

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def admin_action_btn(name, path, option = {})
     link_to(name, path, option.merge(class: "btn btn-primary"))
   end
+
+  def admin_action_btn_sm(name, path, option = {})
+    link_to(name, path, option.merge(class: "btn btn-primary btn-sm"))
+  end
 end
