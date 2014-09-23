9cfd4380a89bae476320cf335289ff2dd93bfc34
やましー <3.1415mailadress@gmail.com>
2014-09-23 16:39:28 +0900

    Submitボタンにfontawesome適用

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,14 +12,18 @@ module ApplicationHelper
   end
 
   def admin_destroy_btn(path, option = {})
-    link_to('<i class="fa fa-trash-o"></i> Destroy'.html_safe, path, option.merge(class: "btn btn-danger", method: :delete, data: { confirm: 'Are you sure?' }))
+    link_to(fa_icon("trash-o", text: 'Destroy'), path, option.merge(class: "btn btn-danger", method: :delete, data: { confirm: 'Are you sure?' }))
   end
 
   def admin_edit_btn(path, option = {})
-    link_to('<i class="fa fa-pencil"></i> Edit'.html_safe, path, option.merge(class: "btn btn-primary"))
+    link_to(fa_icon("pencil", text: 'Edit'), path, option.merge(class: "btn btn-primary"))
   end
 
   def admin_new_btn(path, option = {})
-    link_to('<i class="fa fa-check"></i> New Beacon'.html_safe, path, option.merge(class: "btn btn-warning"))
+    link_to(fa_icon("check", text: 'New Beacon'), path, option.merge(class: "btn btn-warning"))
+  end
+
+  def admin_submit_btn
+    content_tag(:button, fa_icon("paper-plane-o", text: 'Submit'), class: "btn btn-primary")
   end
 end
