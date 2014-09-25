64ed510ebf436d117edd27452f19a7ecb00eb8d7
やましー <3.1415mailadress@gmail.com>
2014-09-25 23:27:26 +0900

    ボタンのヘルパーをつくった

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,24 +3,34 @@ module ApplicationHelper
     link_to(name, path, class: "list-group-item")
   end
 
-  def admin_action_btn(name, path, option = {})
-    link_to(name, path, option.merge(class: "btn btn-primary"))
-  end
-
-  def admin_action_btn_sm(name, path, option = {})
-    link_to(name, path, option.merge(class: "btn btn-primary btn-sm"))
-  end
-
-  def admin_destroy_btn(path, option = {})
-    link_to(fa_icon("trash-o", text: 'Destroy'), path, option.merge(class: "btn btn-danger", method: :delete, data: { confirm: 'Are you sure?' }))
+  def admin_back_btn(path, option = {})
+    link_to(fa_icon("arrow-left", text: 'Back'), path, option.merge(class: "btn btn-primary"))
   end
 
   def admin_edit_btn(path, option = {})
     link_to(fa_icon("pencil", text: 'Edit'), path, option.merge(class: "btn btn-primary"))
   end
 
+  def admin_edit_btn_sm(path, option = {})
+    link_to(fa_icon("pencil", text: 'Edit'), path, option.merge(class: "btn btn-primary btn-sm"))
+  end
+
+  def admin_destroy_btn(path, option = {})
+    link_to(fa_icon("trash-o", text: 'Destroy'), path, option.merge(class: "btn btn-danger", method: :delete, data: { confirm: 'Are you sure?' }))
+  end
+
+  def admin_destroy_btn_sm(path, option = {})
+    link_to(fa_icon("trash-o", text: 'Destroy'), path, option.merge(class: "btn btn-danger btn-sm", method: :delete, data: { confirm: 'Are you sure?' }))
+  end
+
   def admin_new_btn(path, option = {})
     link_to(fa_icon("check", text: 'New Beacon'), path, option.merge(class: "btn btn-warning"))
+  end
+
+  # TODO: admin_update_btnつくる
+
+  def admin_show_btn(path, option = {})
+    link_to(fa_icon("list-ul", text: 'Show'), path, option.merge(class: "btn btn-primary"))
   end
 
   def admin_submit_btn
