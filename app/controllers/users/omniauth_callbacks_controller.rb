e49cf40aa4b7d4ac164fb4f468052d498861b00f
izumin5210 <masayuki@izumin.info>
2014-09-26 22:03:02 +0900

    Add authenticate user from token to API::BaseController

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,23 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    authenticate
+  end
+
+  def twitter
+    authenticate
+  end
+
+  private
+
+    def authenticate
+      auth = request.env["omniauth.auth"]
+      user = User.find_by_auth_params(auth)
+      if user_signed_in?
+        current_user.add_identity(auth) if user.blank?
+      else
+        user = User.create_from_omniauth(auth) if user.blank?
+        sign_in user
+      end
+      # TODO: どっかに遷移させないと死ぬ
+    end
+end
