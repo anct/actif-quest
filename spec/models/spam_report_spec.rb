33319d4ad019eddb7c0f349ddcbd913b8f172d8b
やましー <3.1415mailadress@gmail.com>
2014-09-30 22:41:49 +0900

    スキーマ情報更新

--- spec/models/spam_report_spec.rb
+++ spec/models/spam_report_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: spam_reports
+#
+#  id          :integer          not null, primary key
+#  reporter_id :integer
+#  spam_id     :integer
+#  spam_type   :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe SpamReport, :type => :model do
