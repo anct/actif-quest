3a23819ff0971252e7d5c39eececb766db56ba4b
izumin5210 <masayuki@izumin.info>
2014-10-06 23:50:03 +0900

    Add validations and specs to check_in model

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -14,4 +14,6 @@ class CheckIn < ActiveRecord::Base
   belongs_to :bound
   belongs_to :user
 
+  validates_presence_of :bound_id, :user_id
+  validates_uniqueness_of :user_id, scope: :bound_id
 end
