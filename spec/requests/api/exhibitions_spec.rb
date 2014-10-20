79ef1c20676e7252a3715518c219a42bf6396c9e
izumin5210 <masayuki@izumin.info>
2014-10-20 12:06:12 +0900

    Add request spec of error to GET /api/exhibition/:id

--- spec/requests/api/exhibitions_spec.rb
+++ spec/requests/api/exhibitions_spec.rb
@@ -18,20 +18,37 @@ RSpec.describe 'Exhibitions API', type: :request do
   end
 
   describe 'GET /api/exhibitions/:id' do
-    let(:exhibition) { exhibitions[0] }
-    let(:id) { exhibitions[0].id }
     context 'w/ Authozization header' do
       before { sign_in_as_user_with_token }
+      context 'w/ valid exhibition id' do
+        let(:exhibition) { exhibitions[0] }
+        let(:id) { exhibitions[0].id }
 
-      it 'returns the exhibition', :autodoc do
-        is_expected.to eq 200
-        json = response.body
-        expect(json).to have_json_type(Hash)
-        expect(json).to be_json_eql(exhibition.id).at_path('id')
-        expect(json).to be_json_eql("\"#{exhibition.name}\"").at_path('name')
-        expect(json).to be_json_eql("\"#{exhibition.introduction}\"").at_path('introduction')
-        expect(json).to include_json(exhibition.group.to_json)
-        expect(json).to have_json_path('image')
+        it 'returns the exhibition', :autodoc do
+          is_expected.to eq 200
+          json = response.body
+          expect(json).to have_json_type(Hash)
+          expect(json).to be_json_eql(exhibition.id).at_path('id')
+          expect(json).to be_json_eql("\"#{exhibition.name}\"").at_path('name')
+          expect(json).to be_json_eql("\"#{exhibition.introduction}\"").at_path('introduction')
+          expect(json).to include_json(exhibition.group.to_json)
+          expect(json).to have_json_path('image')
+        end
+      end
+
+      context 'w/ invalid exhibition id' do
+        let(:id) do
+          id = exhibitions.last.id
+          ids = Exhibition.pluck(:id)
+          loop { return id unless ids.include?(id+=1) }
+        end
+        it 'returns 404 not found' do
+          is_expected.to eq 404
+          json = response.body
+          expect(json).to have_json_path('error')
+          expect(json).to have_json_path('error/message')
+          expect(json).to be_json_eql(%("That exhibition does not exist.")).at_path('error/message')
+        end
       end
     end
   end
@@ -62,7 +79,7 @@ RSpec.describe 'Exhibitions API', type: :request do
         end
       end
 
-      context 'w/ valid exhibition id' do
+      context 'w/ invalid exhibition id' do
         let(:id) do
           id = exhibitions.last.id
           ids = Exhibition.pluck(:id)
