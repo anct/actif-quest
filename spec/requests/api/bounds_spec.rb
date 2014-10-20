e1956aca04f43241396a2f1fa3654413ca44beb8
izumin5210 <masayuki@izumin.info>
2014-10-20 09:56:40 +0900

    Add request spec for /api/bounds/:id/check_in

--- spec/requests/api/bounds_spec.rb
+++ spec/requests/api/bounds_spec.rb
@@ -16,4 +16,33 @@ RSpec.describe 'Bounds API', type: :request do
       end
     end
   end
+
+  describe 'POST /api/bounds/:id/check_in' do
+    context 'w/ Authorization header' do
+      before { @user = sign_in_as_user_with_token }
+      context 'w/ valid beacon id' do
+        let(:id) { bounds[0].id }
+        it 'returns 201 created', :autodoc do
+          is_expected.to eq 201
+          expect(response.body).to_not have_json_path('error')
+          expect(@user.check_ins.where(bound: bounds[0])).to be_present
+        end
+      end
+
+      context 'w/ invalid beacon id' do
+        let(:id) do
+          id = bounds.last.id
+          ids = Bound.pluck(:id)
+          loop { return id unless ids.include?(id+=1) }
+        end
+        it 'returns 404 not found' do
+          is_expected.to eq 404
+          json = response.body
+          expect(json).to have_json_path('error')
+          expect(json).to have_json_path('error/message')
+          expect(json).to be_json_eql(%("That bound does not exist.")).at_path('error/message')
+        end
+      end
+    end
+  end
 end
