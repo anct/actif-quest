8e21093211bf3a3a6ddf70b59a6a7256272d3bfc
やましー <3.1415mailadress@gmail.com>
2014-10-31 20:32:02 +0900

    Add images to Treasures-api responses

--- app/serializers/treasure_serializer.rb
+++ app/serializers/treasure_serializer.rb
@@ -1,6 +1,10 @@
 class TreasureSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :image_url
+  attributes :id, :name, :description, :image
 
   has_one :bound
+
+  def image
+    Hash[object.image_url.versions.map{ |key, image| [key, image.url] }]
+  end
   
 end
