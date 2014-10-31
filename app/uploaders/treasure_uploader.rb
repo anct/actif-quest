9d039940f24deb2d41f92c20a6bdc66b9d26689c
やましー <3.1415mailadress@gmail.com>
2014-10-31 19:11:07 +0900

    Set Treasure's image size

--- app/uploaders/treasure_uploader.rb
+++ app/uploaders/treasure_uploader.rb
@@ -32,9 +32,9 @@ class TreasureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :medium do
+    process :resize_to_fill => [1080, 840]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
