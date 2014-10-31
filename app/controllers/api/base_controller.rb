b819d9c47b8820305a60ef98f9f494aaf767c8ba
izumin5210 <masayuki@izumin.info>
2014-10-31 17:53:00 +0900

    Skip verifying authenticity token when api request

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,4 +1,5 @@
 class Api::BaseController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   before_action :authenticate_user_from_token!
   before_filter :authenticate_user!
 
