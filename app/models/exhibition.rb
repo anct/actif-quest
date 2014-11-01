2cc85f6801c1aacb4812d324fe5ce6763473d8d6
izumin5210 <masayuki@izumin.info>
2014-11-01 11:34:48 +0900

    Modify validations

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -20,7 +20,6 @@ class Exhibition < ActiveRecord::Base
   belongs_to :group
 
   validates :name, presence: true
-  validates :introduction, presence: true
   validates :group_id, presence: true
 
   mount_uploader :image_url, ExhibitionUploader
